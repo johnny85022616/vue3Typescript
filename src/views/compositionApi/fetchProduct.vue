5559445cfa6c712616a48092caba9a8af5091c89
johnny_chang <johnny_chang@friday.tw>
2023-01-04 17:49:38 +0800

    test

--- src/views/compositionApi/fetchProduct.vue
+++ src/views/compositionApi/fetchProduct.vue
@@ -2,23 +2,25 @@
   <div class="product">
     <h1>vue3 typescript fetch practice</h1>
     <div class="product__item">
-      <p>品名:</p>
+      <!-- <p>品名:{{ productJson.data[0].name }}</p>
+      <p v-html="productJson.data[0].intro"></p> -->
+      <p>{{ productJson }}</p>
     </div>
   </div>
 </template>
   
 <script setup lang="ts">
 import { getProduct } from '@/api/product/productDetail';
-import { reactive,toRefs } from 'vue';
+import { reactive } from 'vue';
 import { IProduct } from '../../types/product';
- 
+
 
 let productJson = reactive<IProduct>({})   
 
 async function getProductDetail(): Promise<void> {
   const product = await getProduct()
   productJson = product
-  console.log(productJson);
+  console.log(product , productJson);
 }
 
 getProductDetail()
